1f344acdacd2ea1a8f46f0e3da3302e2eacdb48d
Paul Epperson <pepperson00@gmail.com>
2019-07-14 22:45:10 -0700

    Initial exports

---
+++ index.js
@@ -0,0 +1,20 @@
+exports.validate_state_update_local = function (original, update) {
+    console.log("Validating state update") 
+
+    // TODO
+    return true
+}
+
+exports.attempt_state_update = function(original, update) {
+    console.log("Attempting state update")
+
+    // TODO
+    return true
+}
+
+exports.process_updates = function (updates) {
+    console.log("Processing updates") 
+
+    // TODO
+    return true
+}
