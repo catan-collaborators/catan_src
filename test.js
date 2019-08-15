71c1fcc91679cd73260af8b726f2d1a33614e4a1
Paul Epperson <pepperson00@gmail.com>
2019-08-15 04:56:57 -0700

    Test changes, added some utilities

--- test.js
+++ test.js
@@ -1,16 +1,124 @@
 const catan_ig = require('./index')
-const _ = require('underscore')
 
-function assert_state_updates_local(state_update_name, validated, expected, initial, updates)
+const validate_test_utilities = true
+
+class TestError extends Error
 {
-    if (!validated)
+    constructor(...args)
     {
-        console.error(`Failed to assert ${state_update_name}`)
-        console.error(`Expected: ${JSON.parse(expected, null, )}`)
-        console.error(`Initial: ${JSON.parse(expected, null, )}`)
-        _.each(updates, function (element, index, list) { console.error(`Update ${index+1}: ${element}`) })
-        throw `Failed to assert ${state_update_name}`
+        super(...args)
+        Error.captureStackTrace(this, this.constructor)
     }
+}
+
+function expect_error(f, success = null, match = null, assertion = null)
+{
+    let result
+
+    try
+    {
+        result = f()
+    }
+    catch (error)
+    {
+        if (match == null || error.message == match)
+        {
+            if (success != null)
+            {
+                console.log(success)
+            }
+
+            return true
+        }
+
+        let message = `Failed to throw expected error`
+        message += `${"\n"}Exception was ${error.toString()}`
+        throw new TestError(message)
+    }
+
+    let message = `Failed to throw any error`
+    message += `${"\n"}Result was ${result}`
+
+    if (expected == null)
+    {
+        throw new TestError(message)
+    }
+
+    message += `${"\n"}Failed to throw on ${assertion}`
+    throw new TestError(message)
+}
+
+function assert_state_updates_local(state_update_name, result, expected, initial, updates)
+{
+    if (typeof state_update_name !== "string")
+    {
+        throw new TestError(`Bad 'state_update_name' variable: ${state_update_name}`)
+    }
+
+    if (typeof result !== "boolean")
+    {
+        throw `Bad 'result' variable: ${validated}`
+    }
+
+    if (typeof expected !== "boolean")
+    {
+        throw `Bad 'expected' variable: ${expected}`
+    }
+
+    if (result != expected)
+    {
+        const json_indent = 4
+        let error = TestError()
+        error.message = `Failed to assert '${state_update_name}'`
+        error += `${"\n"}Expected '${expected}' but got '${result}'`
+
+        if (initial == null || typeof initial !== 'object')
+        {
+            error += `Initial: ${initial}`
+        }
+        else
+        {
+            error += `Initial: ${JSON.stringify(initial, null, json_indent)}`
+        }
+
+        if (updates == null || !Array.isArray(updates))
+        {
+            error += `Updates: ${updates}`
+        }
+        else
+        {
+            error += `Updates:`
+            updates
+                .forEach((value, index) =>
+                {
+                    if (value == null || typeof value !== 'object')
+                    {
+                        error += `Update ${index+1}: ${value}`
+                    }
+                    else
+                    {
+                        error += `Update ${index+1}: ${JSON.stringify(value, null, json_indent)}`
+                    }
+                })
+        }
+
+        throw error
+    }
+}
+
+if (validate_test_utilities)
+{
+    console.log("Validating test utilities")
+    expect_error(
+        () => assert_state_updates_local(null, null, null, null, null),
+        "Validated null 'state_update_name' variable for assert_state_updates_local throws error",
+        "Bad 'state_update_name' variable: null")
+    expect_error(
+        () => assert_state_updates_local(5, null, null, null, null),
+        "Validated null 'result' variable for assert_state_updates_local must be string",
+        "Bad 'state_update_name' variable: 5")
+    expect_error(() => assert_state_updates_local(5, null, null, null, null))
+    console.log("")
 }
 
 console.log("Validating local state updates")
