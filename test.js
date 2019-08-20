926557b0226aeb918d96cc06cc057bd0c72c4cc1
sonpeppers <pepperson00@gmail.com>
2019-08-20 00:54:21 -0700

    Added expect_no_error, updated assert_state_updates_local, fixed tests

--- test.js
+++ test.js
@@ -1,29 +1,79 @@
 const catan_ig = require('./index')
 
-const counter = {} 
-counter.val = 0
-
 const validate_test_utilities = true
 const json_indent = 4
 
-function expect_error(f, success = null, match = null, assertion = undefined)
+const counter = { 'val': 0 }
+
+function expect_no_error(func, expected, success = null, assertion = null)
 {
     let result
 
     try
     {
-        result = f()
+        result = func()
+    }
+    catch (error)
+    {
+        let message = `\x1b[31mUnexpectedly threw error`
+        message += `${"\n"}Function call was ${func.toString()}`
+        message += `${"\n"}Exception was: ${error.message}`
+
+        if (assertion != null)
+        {
+            message += `${"\n"}Assertion was: ${assertion}`
+        }
+
+        message += `\x1b[0m`
+        error.message = message
+        throw error
+    }
+
+    if (match != result)
+    {
+        let message = `\x1b[31mResult did not match expected value`
+        message += `${"\n"}Function call was ${func.toString()}`
+        message += `${"\n"}Expected value was ${expected}`
+        message += `${"\n"}Returned value was ${result}`
+
+        if (assertion != null)
+        {
+            message += `${"\n"}Assertion was: ${assertion}`
+        }
+
+        message += `\x1b[0m`
+        error.message = message
+        throw new Error(message)
+    }
+
+    if (success != null)
+    {
+        console.log(`\x1b[32m${success}\x1b[0m`)
+    }
+
+    counter.val++
+    return true
+}
+
+function expect_error(func, success = null, match = null, assertion = undefined)
+{
+    let result
+
+    try
+    {
+        result = func()
     }
     catch (error)
     {
         if (match != null && error.message != match)
         {
             let message = `\x1b[31mFailed to throw expected error`
-            message += `${"\n"}Exception was ${error.message}`
+            message += `${"\n"}Function call was ${func.toString()}`
+            message += `${"\n"}Exception was: ${error.message}`
 
             if (assertion != null)
             {
-                message += `${"\n"}Assertion was ${assertion}`
+                message += `${"\n"}Assertion was: ${assertion}`
             }
 
             message += `\x1b[0m`
@@ -46,6 +96,7 @@ function expect_error(f, success = null, match = null, assertion = undefined)
     }
 
     let message = `\x1b[31mFailed to throw any error`
+    message += `${"\n"}Function call was ${func.toString()}`
     message += `${"\n"}Result was ${result == null ? result : result.toString()}`
 
     if (assertion != null)
@@ -57,22 +108,24 @@ function expect_error(f, success = null, match = null, assertion = undefined)
     throw new Error(message)
 }
 
-function assert_state_updates_local(state_update_name, result, expected, initial, updates)
+function assert_state_updates_local(state_update_name, action, expected, initial, updates)
 {
     if (typeof state_update_name !== "string")
     {
         throw new Error(`Bad 'state_update_name' variable: ${state_update_name == null ? state_update_name : state_update_name.toString()}`)
     }
 
-    if (typeof result !== "boolean")
+    if (typeof action !== "function")
     {
-        throw new Error(`Bad 'result' variable: ${result == null ? result : result.toString()}`)
+        throw new Error(`Bad 'result' variable: ${action == null ? action : action.toString()}`)
     }
 
     if (typeof expected !== "boolean")
     {
         throw new Error(`Bad 'expected' variable: ${expected == null ? expected : expected.toString()}`)
     }
+
+    let result = action(initial, updates)
 
     if (result != expected)
     {
@@ -127,130 +180,130 @@ if (validate_test_utilities)
     let str_var = "hello world"
     let obj_var = {}
     let sym_var = Symbol()
-    let fun_var = () => {;}
+    let func_var = () => {;}
 
     expect_error(
         () => assert_state_updates_local(undefined_var, null, null, null, null),
         `Validated non-string (${typeof undefined_var}) value '${undefined_var}' for 'state_update_name' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'state_update_name' variable: undefined",
-        "Assert 'state_update_name' in function 'assert_state_updates_local' is not undefined")
+        "Assert 'state_update_name' variable in function 'assert_state_updates_local' is not undefined")
     expect_error(
         () => assert_state_updates_local(null_var, null, null, null, null),
         `Validated non-string (null) value '${null_var}' for 'state_update_name' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'state_update_name' variable: null",
-        "Assert 'state_update_name' in function 'assert_state_updates_local' is not null")
+        "Assert 'state_update_name' variable in function 'assert_state_updates_local' is not null")
     expect_error(
         () => assert_state_updates_local(nan_var, null, null, null, null),
         `Validated non-string (${typeof nan_var}) value '${nan_var}' for 'state_update_name' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'state_update_name' variable: NaN",
-        "Assert 'state_update_name' in function 'assert_state_updates_local' is not number (NaN)")
+        "Assert 'state_update_name' variable in function 'assert_state_updates_local' is not number (NaN)")
     expect_error(
         () => assert_state_updates_local(num_var, null, null, null, null),
         `Validated non-string (${typeof num_var}) value '${num_var}' for 'state_update_name' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'state_update_name' variable: 5",
-        "Assert 'state_update_name' in function 'assert_state_updates_local' is not number")
+        "Assert 'state_update_name' variable in function 'assert_state_updates_local' is not number")
     expect_error(
         () => assert_state_updates_local(bool_var, null, null, null, null),
         `Validated non-string (${typeof bool_var}) value '${bool_var}' for 'state_update_name' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'state_update_name' variable: false",
-        "Assert 'state_update_name' in function 'assert_state_updates_local' is not boolean")
+        "Assert 'state_update_name' variable in function 'assert_state_updates_local' is not boolean")
     expect_error(
         () => assert_state_updates_local(obj_var, null, null, null, null),
         `Validated non-string (${typeof obj_var}) value '${obj_var}' for 'state_update_name' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'state_update_name' variable: [object Object]",
-        "Assert 'state_update_name' in function 'assert_state_updates_local' is not object")
+        "Assert 'state_update_name' variable in function 'assert_state_updates_local' is not object")
     expect_error(
         () => assert_state_updates_local(sym_var, null, null, null, null),
         `Validated non-string (${typeof sym_var}) value '${sym_var.toString()}' for 'state_update_name' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'state_update_name' variable: Symbol()",
-        "Assert 'state_update_name' in function 'assert_state_updates_local' is not symbol")
+        "Assert 'state_update_name' variable in function 'assert_state_updates_local' is not symbol")
     expect_error(
-        () => assert_state_updates_local(fun_var, null, null, null, null),
-        `Validated non-string (${typeof fun_var}) value '${fun_var}' for 'state_update_name' variable for function 'assert_state_updates_local' throws error`,
+        () => assert_state_updates_local(func_var, null, null, null, null),
+        `Validated non-string (${typeof func_var}) value '${func_var}' for 'state_update_name' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'state_update_name' variable: () => {;}",
-        "Assert 'state_update_name' in function 'assert_state_updates_local' is not function")
+        "Assert 'state_update_name' variable in function 'assert_state_updates_local' is not function")
 
     expect_error(
         () => assert_state_updates_local("Test case 1", undefined_var, null, null, null),
-        `Validated non-boolean (${typeof undefined_var}) value '${undefined_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
+        `Validated non-function (${typeof undefined_var}) value '${undefined_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'result' variable: undefined",
-        "Assert 'result' in function 'assert_state_updates_local' is not undefined")
+        "Assert 'result' variable in function 'assert_state_updates_local' is not undefined")
     expect_error(
         () => assert_state_updates_local("Test case 1", null_var, null, null, null),
-        `Validated non-boolean (null) value '${null_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
+        `Validated non-function (null) value '${null_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'result' variable: null",
-        "Assert 'result' in function 'assert_state_updates_local' is not null")
+        "Assert 'result' variable in function 'assert_state_updates_local' is not null")
     expect_error(
         () => assert_state_updates_local("Test case 1", nan_var, null, null, null),
-        `Validated non-boolean (${typeof nan_var}) value '${nan_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
+        `Validated non-function (${typeof nan_var}) value '${nan_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'result' variable: NaN",
-        "Assert 'result' in function 'assert_state_updates_local' is not number (NaN)")
+        "Assert 'result' variable in function 'assert_state_updates_local' is not number (NaN)")
     expect_error(
         () => assert_state_updates_local("Test case 1", num_var, null, null, null),
-        `Validated non-boolean (${typeof num_var}) value '${num_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
+        `Validated non-function (${typeof num_var}) value '${num_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'result' variable: 5",
-        "Assert 'result' in function 'assert_state_updates_local' is not number")
+        "Assert 'result' variable in function 'assert_state_updates_local' is not number")
+    expect_error(
+        () => assert_state_updates_local("Test case 1", bool_var, null, null, null),
+        `Validated non-function (${typeof bool_var}) value '${bool_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
+        "Bad 'result' variable: false",
+        "Assert 'result' variable in function 'assert_state_updates_local' is not boolean")
     expect_error(
         () => assert_state_updates_local("Test case 1", str_var, null, null, null),
-        `Validated non-boolean (${typeof str_var}) value '${str_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
+        `Validated non-function (${typeof str_var}) value '${str_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'result' variable: hello world",
-        "Assert 'result' in function 'assert_state_updates_local' is not string")
+        "Assert 'result' variable in function 'assert_state_updates_local' is not string")
     expect_error(
         () => assert_state_updates_local("Test case 1", obj_var, null, null, null),
-        `Validated non-boolean (${typeof obj_var}) value '${obj_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
+        `Validated non-function (${typeof obj_var}) value '${obj_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'result' variable: [object Object]",
-        "Assert 'result' in function 'assert_state_updates_local' is not object")
+        "Assert 'result' variable in function 'assert_state_updates_local' is not object")
     expect_error(
         () => assert_state_updates_local("Test case 1", sym_var, null, null, null),
-        `Validated non-boolean (${typeof sym_var}) value '${sym_var.toString()}' for 'result' variable for function 'assert_state_updates_local' throws error`,
+        `Validated non-function (${typeof sym_var}) value '${sym_var.toString()}' for 'result' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'result' variable: Symbol()",
-        "Assert 'result' in function 'assert_state_updates_local' is not symbol")
-    expect_error(
-        () => assert_state_updates_local("Test case 1", fun_var, null, null, null),
-        `Validated non-boolean (${typeof fun_var}) value '${fun_var}' for 'result' variable for function 'assert_state_updates_local' throws error`,
-        "Bad 'result' variable: () => {;}",
-        "Assert 'result' in function 'assert_state_updates_local' is not symbol")
+        "Assert 'result' variable in function 'assert_state_updates_local' is not symbol")
 
     expect_error(
-        () => assert_state_updates_local("Test case 1", true, undefined_var, null, null),
+        () => assert_state_updates_local("Test case 1", catan_ig.validate_state_updates_local, undefined_var, null, null),
         `Validated non-boolean (${typeof undefined_var}) value '${undefined_var}' for 'expected' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'expected' variable: undefined",
-        "Assert 'expected' in function 'assert_state_updates_local' is not undefined")
+        "Assert 'expected' variable in function 'assert_state_updates_local' is not undefined")
     expect_error(
-        () => assert_state_updates_local("Test case 1", true, null_var, null, null),
+        () => assert_state_updates_local("Test case 1", catan_ig.validate_state_updates_local, null_var, null, null),
         `Validated non-boolean (null) value '${null_var}' for 'expected' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'expected' variable: null",
-        "Assert 'expected' in function 'assert_state_updates_local' is not null")
+        "Assert 'expected' variable in function 'assert_state_updates_local' is not null")
     expect_error(
-        () => assert_state_updates_local("Test case 1", true, nan_var, null, null),
+        () => assert_state_updates_local("Test case 1", catan_ig.validate_state_updates_local, nan_var, null, null),
         `Validated non-boolean (${typeof nan_var}) value '${nan_var}' for 'expected' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'expected' variable: NaN",
-        "Assert 'expected' in function 'assert_state_updates_local' is not number (NaN)")
+        "Assert 'expected' variable in function 'assert_state_updates_local' is not number (NaN)")
     expect_error(
-        () => assert_state_updates_local("Test case 1", true, num_var, null, null),
+        () => assert_state_updates_local("Test case 1", catan_ig.validate_state_updates_local, num_var, null, null),
         `Validated non-boolean (${typeof num_var}) value '${num_var}' for 'expected' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'expected' variable: 5",
-        "Assert 'expected' in function 'assert_state_updates_local' is not number")
+        "Assert 'expected' variable in function 'assert_state_updates_local' is not number")
     expect_error(
-        () => assert_state_updates_local("Test case 1", true, str_var, null, null),
+        () => assert_state_updates_local("Test case 1", catan_ig.validate_state_updates_local, str_var, null, null),
         `Validated non-boolean (${typeof str_var}) value '${str_var}' for 'expected' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'expected' variable: hello world",
-        "Assert 'expected' in function 'assert_state_updates_local' is not string")
+        "Assert 'expected' variable in function 'assert_state_updates_local' is not string")
     expect_error(
-        () => assert_state_updates_local("Test case 1", true, obj_var, null, null),
+        () => assert_state_updates_local("Test case 1", catan_ig.validate_state_updates_local, obj_var, null, null),
         `Validated non-boolean (${typeof obj_var}) value '${obj_var}' for 'expected' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'expected' variable: [object Object]",
-        "Assert 'expected' in function 'assert_state_updates_local' is not object")
+        "Assert 'expected' variable in function 'assert_state_updates_local' is not object")
     expect_error(
-        () => assert_state_updates_local("Test case 1", true, sym_var, null, null),
+        () => assert_state_updates_local("Test case 1", catan_ig.validate_state_updates_local, sym_var, null, null),
         `Validated non-boolean (${typeof sym_var}) value '${sym_var.toString()}' for 'expected' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'expected' variable: Symbol()",
-        "Assert 'expected' in function 'assert_state_updates_local' is not symbol")
+        "Assert 'expected' variable in function 'assert_state_updates_local' is not symbol")
     expect_error(
-        () => assert_state_updates_local("Test case 1", true, fun_var, null, null),
-        `Validated non-boolean (${typeof fun_var}) value '${fun_var}' for 'expected' variable for function 'assert_state_updates_local' throws error`,
+        () => assert_state_updates_local("Test case 1", catan_ig.validate_state_updates_local, func_var, null, null),
+        `Validated non-boolean (${typeof func_var}) value '${func_var}' for 'expected' variable for function 'assert_state_updates_local' throws error`,
         "Bad 'expected' variable: () => {;}",
-        "Assert 'expected' in function 'assert_state_updates_local' is not symbol")
+        "Assert 'expected' variable in function 'assert_state_updates_local' is not symbol")
 
     console.log(`${counter.val} ${counter.val == 1 ? 'test' : 'tests'} asserted`)
     console.log("")
